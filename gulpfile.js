6c5c1a8d15fe659aa393a0dbc75439defa9ddb40
Alexsander Krylov <info@lexistudio.ru>
2015-11-29 14:09:41 +0400

    update gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,33 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var requirejsOptimize = require('gulp-requirejs-optimize');
- 
-gulp.task('css', function () {
+var sftp = require('gulp-sftp');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('css-less', function () {
   return gulp.src('./src/css/app.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./www/css'));
+        .pipe(less({paths: [ path.join(__dirname, 'less', 'includes') ]}))
+        .pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('css', ["css-less"], function() {
+  return gulp.src('./www/css/*.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./www/css/'));
 });
 
 gulp.task('js', function () {
-    return gulp.src('./src/js/app.js')
+  return gulp.src('./src/js/app.js')
         .pipe(requirejsOptimize())
         .pipe(gulp.dest('./www/js'));
+});
+ 
+gulp.task('deploy', function () {
+  return gulp.src('./www/**')
+        .pipe(sftp({
+          host: '',
+          user: '',
+          pass: '',
+          remotePath: '/home/'
+        }));
 });
